eba198b4032584be9dc6c67a7fa33c853cbe5a15
Yuhang GE <abraham1@163.com>
2017-06-07 14:49:37 +0800

    init

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,29 @@
 package com.hansight.analysis
 
+import io.vertx.core.AbstractVerticle
+import io.vertx.ext.web.Router
+
 fun getGreeting(): String {
   return "Hello, World!"
 }
 
-fun main(args: Array<String>) {
-  println(getGreeting())
+class MainVerticle : AbstractVerticle() {
+
+  override fun start() {
+
+    val router = Router.router(vertx)
+
+    router.get("/").handler({
+      it.response().end("Hello, World!")
+    })
+    router.get("/ok").handler({
+      it.response().end("Ok!")
+    })
+
+    vertx.createHttpServer()
+        .requestHandler({
+          router.accept(it)
+        }).listen(8080)
+  }
+
 }
